c42b272c508d2ce1a16c6f10f2eb9cb8a94a3aef
Chandler Severson <me@cseverson.com>
2019-12-11 00:34:54 -0800

    Put tower handling logic in game loop

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const creepHandler = require('creepHandler');
+const towerHandler = require('towerHandler');
 
 module.exports.loop = function () {
     creepHandler.spawnCreeps();
     creepHandler.workCreeps();
     creepHandler.clearMemory();
+    towerHandler.handleAllTowers();
 };
